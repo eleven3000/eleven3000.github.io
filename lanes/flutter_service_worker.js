9eb121d7ab7f3678dfeb3f0d07daf6783811cc4b
eleven3000 <>
2022-01-28 18:31:27 +0000

    Update from https://github.com/eleven3000/lanes/commit/7f2dc11f1d89554bdecbe9bfebc27a781ba250e6

--- lanes/flutter_service_worker.js
+++ lanes/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "bdfe09b95f461ba287699e933b92513b",
-"/": "bdfe09b95f461ba287699e933b92513b",
+  "index.html": "867316e12fc1d2011a5c1b23153a3b4b",
+"/": "867316e12fc1d2011a5c1b23153a3b4b",
 "version.json": "5fe1938474c764d1e431abb5bc8e815a",
 "manifest.json": "429f0ea8ab3f8a64d51037ce619e872b",
-"main.dart.js": "e30e113bd62652f17c1d7ad72679e266",
+"main.dart.js": "abfd5abeb4525afaf6cc7efaf30bf01c",
 "canvaskit/profiling/canvaskit.wasm": "a9610cf39260f60fbe7524a785c66101",
 "canvaskit/profiling/canvaskit.js": "f3bfccc993a1e0bfdd3440af60d99df4",
 "canvaskit/canvaskit.wasm": "04ed3c745ff1dee16504be01f9623498",
@@ -28,7 +28,7 @@ const RESOURCES = {
 "assets/AssetManifest.json": "1980d07daf84716aa16abcd35505c712",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"assets/NOTICES": "79f837d48e5dce842cff5436af33ebe6",
+"assets/NOTICES": "88bd927ff0d75cc2fd74f1de1b644ea4",
 "assets/lib/assets/splash.PNG": "9d52a3d42e31dae5dd82c80740e49473",
 "assets/lib/assets/fonts/Roboto-Medium.ttf": "b2d307df606f23cb14e6483039e2b7fa",
 "assets/lib/assets/bus_animated.json": "39adb998dd2f54e8f5f6645d6cf36a2b"
