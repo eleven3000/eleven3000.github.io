822066c8a79a370be4fd2ba49ff227d9118603fe
eleven3000 <>
2022-01-28 20:22:10 +0000

    Update from https://github.com/eleven3000/lanes/commit/6807fff09880ccabcc1dd902186df99216dd17d0

--- lanes/flutter_service_worker.js
+++ lanes/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "867316e12fc1d2011a5c1b23153a3b4b",
-"/": "867316e12fc1d2011a5c1b23153a3b4b",
+  "index.html": "02b5b86d1391e1909cf202b62e9e7574",
+"/": "02b5b86d1391e1909cf202b62e9e7574",
 "version.json": "5fe1938474c764d1e431abb5bc8e815a",
 "manifest.json": "429f0ea8ab3f8a64d51037ce619e872b",
-"main.dart.js": "abfd5abeb4525afaf6cc7efaf30bf01c",
+"main.dart.js": "959c2370ea4b470932f00980e543c2a7",
 "canvaskit/profiling/canvaskit.wasm": "a9610cf39260f60fbe7524a785c66101",
 "canvaskit/profiling/canvaskit.js": "f3bfccc993a1e0bfdd3440af60d99df4",
 "canvaskit/canvaskit.wasm": "04ed3c745ff1dee16504be01f9623498",
